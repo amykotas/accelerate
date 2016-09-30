7a466b33343940b29e45c94e13d2efb818b3bc68
Amy Kotas <aekotas@gmail.com>
2016-09-30 07:19:02 -0400

    Updated case studies template.

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -14,15 +14,40 @@
 
 get_header(); ?>
 
-  <div id="primary" class="site-content">
-    <div id="content" role="main">
-      <?php while ( have_posts() ) : the_post(); ?>
-      <article class="case-study">
-        <?php the_content(); ?>
-      </article>
-      <?php endwhile; // end of the loop. ?>
+<div id="primary" class="site-content">
+  <div id="content" role="main">
 
-    </div><!-- #content -->
-  </div><!-- #primary -->
+  <?php while ( have_posts() ) : the_post();
+    $services = get_field('services');
+    $client = get_field('client');
+    $link = get_field('site_link');
+    $image_1 = get_field('image_1');
+    $image_2 = get_field('image_2');
+    $image_3 = get_field('image_3');
+    $size = "full"; ?>
 
-<?php get_footer(); ?>
+  <article class="case-study">
+    <aside class="case-study-sidebar">
+      <h2><?php the_title(); ?></h2>
+      <h3><?php echo $services; ?></h3>
+      <h4>Client: <?php echo $client; ?></h4>
+
+      <?php the_content(); ?>
+
+      <p><a href="<?php echo $link; ?>">Visit Live Site</a></p>
+    </aside>
+
+    <div class="case-study-images">
+      <?php if($image_1) { ?>
+        <?php echo wp_get_attachment_image( $image_1, $size ); ?>
+      <?php } ?>
+      <?php if($image_2) { ?>
+        <?php echo wp_get_attachment_image( $image_2, $size ); ?>
+      <?php } ?>
+      <?php if($image_3) { ?>
+        <?php echo wp_get_attachment_image( $image_3, $size ); ?>
+      <?php } ?>
+    </div>
+
+  </article>
+<?php endwhile;  // end of the loop ?>
