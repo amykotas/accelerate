656313213c62b2643e5a181ba56e8aeeae55213f
Amy Kotas <aekotas@gmail.com>
2016-10-17 07:01:27 -0400

    Put custom query into About template, and did more styling.

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -16,18 +16,18 @@ get_header(); ?>
 
 <!-- Gets the hero and intro content for About page -->
 <section class="about-page">
-  <div id="primary" class="site-content">
+  <div class="site-content">
     <?php while ( have_posts() ) : the_post(); ?>
       <div class='about-hero'>
         <?php the_content(); ?>
-        <h1><span id="first-word">Accelerate</span> is a strategy and marketing agency located in the heart of NYC. Our goal is to build businesses by making our clients visible and making their customers smile.</h1>
-      </div><!-- #content -->
+      </div><!-- .about-hero -->
     <?php endwhile; // end of the loop. ?>
-  </div><!-- #primary -->
+  </div><!-- .site-content -->
 </section><!-- .about-page -->
 
 <section class="about-services-intro">
   <div class="services-intro">
+    <!-- You can use custom fields for this title and this paragraph -->
     <h2>OUR SERVICES</h2>
     <p>We take pride in our clients and the content we create
     for them.<br>Here's a brief overview of our offered services.</p>
@@ -35,10 +35,29 @@ get_header(); ?>
 </section>
 
 <!-- List of Services -->
-<section class="services">
+<section class="about-services-list">
   <div class="site-content">
-    <h3></h3>
-  </div>
+    <?php query_posts('posts_per_page=4&post_type=services'); ?>
+    <?php while ( have_posts() ) : the_post();
+      $image_1 = get_field("image_1");
+      $size = "medium";?>
+
+    <div class="about-services-posts">
+      <div class="about-services-icon">
+        <?php echo wp_get_attachment_image($image_1, $size); ?>
+      </div>
+      <div class="services-list-content">
+        <div class="services-list-title">
+          <?php the_title(); ?>
+        </div>
+        <?php the_content(); ?>
+      </div>
+
+    </div>
+
+  </div><!-- end .site-content -->
+  <?php endwhile; //end of the loop. ?>
+  <?php wp_reset_query(); // resets the altered query back to the original ?>
 </section> <!-- end .services -->
 
 
